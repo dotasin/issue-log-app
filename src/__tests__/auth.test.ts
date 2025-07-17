25219a76f190245385742ca7300499ada25088be
Ivan Tasin <ivan.tasin@ifolor.com>
2025-07-17 07:50:26 +0200

    more refactoring

--- src/__tests__/auth.test.ts
+++ src/__tests__/auth.test.ts
@@ -1,4 +1,3 @@
-
 import request from 'supertest';
 import app from '../app';
 import { testUsers, createTestUser, getAuthHeader, assertErrorResponse, assertSuccessResponse } from './utils/testHelpers';
